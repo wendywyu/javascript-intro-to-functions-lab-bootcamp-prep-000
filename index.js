ee99f9ad2ef765d32a07ef72e51198621460bdb2
wendy yu <yu.wendyw@gmail.com>
2018-02-21 07:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,3 +33,9 @@ function sayHiToGrandma(string) {
     return 'I can\'t hear you!'
   }
 }
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string) {
+    return 'YES INDEED!'
+  }
+}
