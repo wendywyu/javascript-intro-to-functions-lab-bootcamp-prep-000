a46d14a5397288f5fcfd78cc18a7fe6c9ce88fb7
wendy yu <yu.wendyw@gmail.com>
2018-02-21 06:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ function logWhisper(string) {
 }
 
 var uppercase = "HELLO!"
- 
+
 uppercase.toUpperCase() === uppercase // true
- 
+
 var lowercase = 'hello!'
- 
+
 lowercase.toLowerCase() === lowercase // true
- 
+
 var mixedCase = 'Hi there!'
- 
+
 mixedCase.toLowerCase() === mixedCase // false
- 
+
 mixedCase.toUpperCase() === mixedCase // false
 
 function sayHiToGrandma(string) {
